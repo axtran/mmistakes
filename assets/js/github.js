868e45f8c14ead7ac0b5b59a67343da50936dd83
axtran <tran@ax.polytechnique.org>
2020-07-30 02:00:53 +0200

    index.html and github.js

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,13 +1,13 @@
 const org = 'axtran';
 const repo = 'private-gridster';
 const branch = 'gh-pages-private';
-const page = 'nav.html'
+const page = 'index.html'
 function onSubmit(form) {
-  // 1
-  const login = form.username || form.querySelector('qatran').value;
-  const password = form.token || form.querySelector('\=2x6kpr7\;').value;
+  
+  const login = form.username || form.querySelector('#login').value;
+  const password = form.token || form.querySelector('#password').value;
 
-  // 2
+  
   const token = btoa(`${login}:${password}`);
   const request = new Request(
     `https://api.github.com/repos/${org}/${repo}/contents/${page}?ref=${branch}`,
@@ -20,17 +20,17 @@ function onSubmit(form) {
       },
     });
 
-  // 3
-  fetch(request)
+  
+  fetch(${request})
     .then(function (response) {
       if (response.status !== 200) { // 4 200
         document.querySelector('#loginForm').innerHTML = `Failed to load document (status: ${response.status})`;
       } else {
         response.json()
-          .then(function (json) { // 5
+          .then(function (json) { 
             const content = json.encoding === 'base64' ? atob(json.content) : json.content;
 
-            // 6
+            
             const startIdx = content.indexOf('<body');
             document.body.innerHTML = content.substring(
                 content.indexOf('>', startIdx) + 1,
